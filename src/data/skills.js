c9adac3424539510939b820a0fe41c3648db889a
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2025-05-17 17:26:17 -0300

    Move static skills data to a proper data file

---
+++ src/data/skills.js
@@ -0,0 +1,112 @@
+
+export const skills = [
+    {
+        skillIcon: 'bx:desktop',
+        skillName: 'Frontend',
+        skillDescription: 'Creando interfaces responsivas y fáciles de usar con frameworks y librerías modernas.',
+        technologiesList: [
+            {
+                techIcon: 'simple-icons:html5',
+                techName: 'HTML'
+            },
+            {
+                techIcon: 'simple-icons:css',
+                techName: 'CSS'
+            },
+            {
+                techIcon: 'simple-icons:javascript',
+                techName: 'Javascript' 
+            },
+            {
+                techIcon: 'simple-icons:typescript',
+                techName: 'Typescript'
+            },
+            {
+                techIcon: 'simple-icons:vuedotjs',
+                techName: 'Vue'
+            },
+            {
+                techIcon: 'simple-icons:vite',
+                techName: 'Vite'
+            },
+            {
+                techIcon: 'simple-icons:vitest',
+                techName: 'Vitest'
+            },
+            {
+                techIcon: 'simple-icons:react',
+                techName: 'React'
+            },
+            {
+                techIcon: 'simple-icons:astro',
+                techName: 'Astro'
+            },
+        ]
+    },
+    {
+        skillIcon: 'bx:data',
+        skillName: 'Backend',
+        skillDescription: 'Desarrollando lógica de servidor, APIs y bases de datos robustas para aplicaciones web eficientes.',
+        technologiesList: [
+            {
+                techIcon: 'simple-icons:mysql',
+                techName: 'MySQL'
+            },
+            {
+                techIcon: 'simple-icons:postgresql',
+                techName: 'PostgreSQL'
+            },
+            {
+                techIcon: 'simple-icons:php',
+                techName: 'PHP'
+            },
+            {
+                techIcon: 'simple-icons:laravel',
+                techName: 'Laravel'
+            },
+            {
+                techIcon: 'simple-icons:nodedotjs',
+                techName: 'NodeJS'
+            },
+            {
+                techIcon: 'simple-icons:express',
+                techName: 'Express'
+            },
+        ]
+    },
+    {
+        skillIcon: 'bx:wrench',
+        skillName: 'Otros',
+        skillDescription: 'Esenciales para flujos de desarrollo, colaboración, diseño y gestión de proyectos.',
+        technologiesList: [
+            {
+                techIcon: 'simple-icons:figma',
+                techName: 'Figma'
+            },
+            {
+                techIcon: 'bx:camera',
+                techName: 'Photoscape'
+            },
+            {
+                techIcon: 'simple-icons:git',
+                techName: 'GIT'
+            },
+            {
+                techIcon: 'simple-icons:github',
+                techName: 'GitHub'
+            },
+            {
+                techIcon: 'simple-icons:postman',
+                techName: 'Postman'
+            },
+            {
+                techIcon: 'simple-icons:netlify',
+                techName: 'Netlify'
+            },
+            {
+                techIcon: 'simple-icons:notion',
+                techName: 'Notion'
+            },
+        ]
+    },
+]
