98706f006b6eb70e9950ff48328b55edcbbe2814
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2025-05-30 15:41:37 -0300

    Fix spelling of services items

---
+++ src/data/services.js
@@ -0,0 +1,30 @@
+
+export const services = [
+    {
+        name: 'Frontend',
+        icon: 'bx:news',
+        content: [
+            `<strong>Aplicar un diseño adaptable</strong> a distintos tipos de dispositivos y versiones de navegadores.`,
+            `<strong>Programación, modificación y mantenimiento</strong> de un sitio web.`,
+            `Consultar API's para conseguir una <strong>conexión entre diferentes servicios y sistemas disponibles</strong>.`,
+        ]
+    },
+    {
+        name: 'Backend',
+        icon: 'bx:terminal',
+        content: [
+            `<strong>Definición, configuración y mantenimiento</strong> de base de datos.`,
+            `<strong>Establecer conexión e interacción entre la base de datos</strong> y las soluciones operativas.`,
+            `<strong>Desarrollo de API</strong> propia y acoplamiento con otras.`,
+        ]
+    },
+    {
+        name: 'Diseño',
+        icon: 'bx:palette',
+        content: [
+            `<strong>Identificar productos</strong> de la competencia <strong>y las tendencias</strong> de la industria.`,
+            `<strong>Desarrollar prototipos y flujos de tareas</strong> basados en las necesidades del usuario.`,
+            `<strong>Lograr un boceto inicial</strong> de acuerdo a las especificaciones dadas.`,
+        ]
+    },
+]
